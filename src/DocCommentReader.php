ad1f46c318d20a26e995586882c95eafe27a16bb
kento-oka <kento.oka@kentoka.com>
2018-04-07 17:18:42 +0900

    いろいろ修正

--- src/DocCommentReader.php
+++ src/DocCommentReader.php
@@ -18,55 +18,54 @@ namespace Fratily\Reflection;
  */
 class DocCommentReader{
 
+    const SUMMARY       = 1;
+    const DESCRIPTION   = 2;
+    const ANNOTATIONS   = 3;
+
+    /**
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * @var string
+     */
+    private $description;
+
     /**
      * @var string[]
      */
-    private $docRows;
-
-    /**
-     * Docコメントを解析しやすいように整形する
-     *
-     * @param   string  $docComment
-     *
-     * @return  string[]
-     */
-    private static function normalizeDocComment(string $docComment){
-        $docComment = preg_replace([
-            "`\A/\*\*+\s*`",
-            "`\s*\*+/\z`"
-        ], "", $docComment);
-
-        $rows   = array_map(function($line){
-            return preg_replace("`\A\*+\s*`", "", trim($line));
-        }, explode(PHP_EOL, strtr($docComment, [
-            "\r\n"  => PHP_EOL,
-            "\r"    => PHP_EOL,
-            "\n"    => PHP_EOL,
-        ])));
-
-        return $rows;
-    }
+    private $annotations;
 
     /**
      * Constructor
      *
      * @param   \ReflectionClass|\ReflectionFunctionAbstract|\ReflectionClassConstant|\ReflectionProperty   $reflection
      */
-    public function __construct($reflection){
-        if(is_object($reflection)
-            && (
-                $reflection instanceof \ReflectionClass
-                || $reflection instanceof \ReflectionFunctionAbstract
-                || $reflection instanceof \ReflectionClassConstant
-                || $reflection instanceof \ReflectionProperty
+    public function __construct(string $comment){
+        $rows = explode(
+            PHP_EOL,
+            strtr(
+                preg_replace(
+                    [
+                        "`\A/\*\*+\s*`",
+                        "`\s*\*+/\z`"
+                    ],
+                    "",
+                    $comment
+                ),
+                ["\r\n" => PHP_EOL, "\r" => PHP_EOL, "\n" => PHP_EOL]
             )
-        ){
-            $this->docRows  = self::normalizeDocComment($reflection->getDocComment());
+        );
 
-            return;
-        }
-
-        throw new \InvalidArgumentException();
+        $this->parse(
+            array_map(
+                function($line){
+                    return preg_replace("`\A\*+\s*`", "", trim($line));
+                },
+                $rows
+            )
+        );
     }
 
     /**
@@ -75,6 +74,100 @@ class DocCommentReader{
      * @return  string
      */
     public function __toString(){
-        return implode(PHP_EOL, $this->docRows);
+        return implode(PHP_EOL, array_merge([
+            $this->getSummary(),
+            "",
+            $this->getDescription(),
+            "",
+
+        ], $this->getAnnotations()));
+    }
+
+    /**
+     * Summaryを取得する
+     *
+     * @return string
+     */
+    public function getSummary(){
+        return $this->summary ?? "";
+    }
+
+    /**
+     * Descriptionを取得する
+     *
+     * @return string
+     */
+    public function getDescription(){
+        return $this->description ?? "";
+    }
+
+    /**
+     * Annotationのリストを取得する
+     *
+     * @return string[]
+     */
+    public function getAnnotations(){
+        return $this->annotations ?? [];
+    }
+
+    /**
+     * コメントをSummary, Description, Annotationsに分ける
+     *
+     * @param   string[]    $rows
+     *
+     * @return  void
+     */
+    private function parse(array $rows){
+        $annotation = "";
+        $type       = self::SUMMARY;
+        $in         = true;
+
+        foreach($rows as $row){
+            if(strpos($row, "@") === 0){
+                $type   = self::ANNOTATIONS;
+                $in     = true;
+
+                if($annotation !== ""){
+                    $this->annotations[]    = trim($annotation);
+                }
+
+                $annotation = $row;
+
+                continue;
+            }
+
+            switch($type){
+                case self::SUMMARY:
+                    if($row !== ""){
+                        if($in){
+                            //  空行でなくSummary範囲内なのでこれはSummary
+                            $this->summary  = $row;
+                            $in             = false;
+                        }else{
+                            //  空行でなくSummary範囲外なのでこれはDescription
+                            $this->description  = $row;
+                            $type               = self::DESCRIPTION;
+                            $in                 = true;
+                        }
+                    }else{
+                        //  空行なのでSummary範囲外に突入
+                        $in = false;
+                    }
+
+                    break;
+                case self::DESCRIPTION:
+                    $this->description  .= PHP_EOL . $row;
+                    break;
+                case self::ANNOTATIONS:
+                    $annotation .= $row !== "" ? PHP_EOL . $row : "";
+                    break;
+            }
+        }
+
+        if("" !== $annotation){
+            $this->annotations[]    = trim($annotation);
+        }
+
+        $this->description  = rtrim($this->description, PHP_EOL);
     }
 }
