63664422939eeed5aebca3871d2685e6a5b15dba
kento-oka <kento-oka@kentoka.com>
2019-09-22 23:11:44 +0900

    新バージョンに向けて。いらない機能を切り捨てていく

--- src/ReflectionCallable.php
+++ src/ReflectionCallable.php
@@ -14,193 +14,205 @@
 namespace Fratily\Reflection;
 
 /**
- * 
+ *
  */
-class ReflectionCallable implements \Reflector{
-    
-    const TYPE_ERROR            = 0;
-    const TYPE_INVOKABLE_OBJECT = 1;
-    const TYPE_CLOSURE          = 2;
-    const TYPE_FUNCTION         = 3;
-    const TYPE_METHOD_STRING    = 4;
-    const TYPE_METHOD_ARRAY     = 5;
-    
+class ReflectionCallable{
+
+    public const TYPE_INVOKABLE_OBJECT          = 0;
+    public const TYPE_CLOSURE                   = 1;
+    public const TYPE_STRING                    = 2;
+    public const TYPE_CLASS_METHOD_STRING       = 3;
+    public const TYPE_CLASS_METHOD_ARRAY        = 4;
+    public const TYPE_CLASS_METHOD_ARRAY_STATIC = 5;
+
     /**
      * @var \ReflectionFunction|\ReflectionMethod
      */
     private $reflection;
-    
+
     /**
      * @var callable
      */
     private $value;
-    
+
     /**
-     * @var string|null
-     */
-    private $class;
-    
-    /**
-     * @var int
+     * @var mixed
      */
     private $type;
-    
+
     /**
-     * Exports
-     * 
-     * @return  void
+     * @var object|null
      */
-    public static function export(callable $callable){
-        echo (string)(new static($callable));
-    }
-    
-    public static function resolveType(callable $callable){
-        if($callable instanceof \Closure){
+    private $object;
+
+    /**
+     * Returns the callable type.
+     *
+     * @param callable $callable The callback value
+     *
+     * @return mixed
+     */
+    public static function resolveType(callable $callable)
+    {
+        if ($callable instanceof \Closure) {
             return self::TYPE_CLOSURE;
-        }else if(is_object($callable)){
+        }
+
+        if (is_object($callable)) {
             return self::TYPE_INVOKABLE_OBJECT;
-        }else if(is_string($callable)){
-            if(($pos = strpos($callable, "::")) !== false){
-                return self::TYPE_METHOD_STRING;
+        }
+
+        if (is_array($callable)) {
+            if (is_string($callable[0])) {
+                return self::TYPE_CLASS_METHOD_ARRAY_STATIC;
             }
-            
-            return self::TYPE_FUNCTION;
-        }else if(is_array($callable)){
-            return self::TYPE_METHOD_ARRAY;
+
+            return self::TYPE_CLASS_METHOD_ARRAY;
         }
-        
-        return self::TYPE_ERROR;
+
+        if (!is_string($callable)) {
+            throw new \LogicException();
+        }
+
+        if (false !== mb_strpos($callable, "::")) {
+            return self::TYPE_CLASS_METHOD_STRING;
+        }
+
+        return self::TYPE_STRING;
     }
-    
+
     /**
-     * Constructor
-     * 
-     * @param   callable    $callable
+     * Constructor.
+     *
+     * @param callable $callable The callable value
      */
-    public function __construct(callable $callable){
-        $this->type     = self::resolveType($callable);
-        $this->value    = $callable;
-        
-        if($this->type === self::TYPE_ERROR){
-            throw new \LogicException;
-        }
-        
-        switch($this->type){
+    public function __construct(callable $callable)
+    {
+        $this->type  = static::resolveType($callable);
+        $this->value = $callable;
+
+        switch ($this->type) {
             case self::TYPE_INVOKABLE_OBJECT:
-                $this->reflection   = new \ReflectionMethod($callable, "__invoke");
-                $this->class        = get_class($callable);
-                break;
-            
-            case self::TYPE_CLOSURE:
-            case self::TYPE_FUNCTION:
-                $this->reflection   = new \ReflectionFunction($callable);
-                break;
-            
-            case self::TYPE_METHOD_STRING:
-                $pos        = strpos($callable, "::");
-                $tmp        = $callable;
-                $callable   = [];
-                $callable[] = substr($tmp, 0, $pos);
-                $callable[] = substr($tmp, $pos + 2);
-                
-            case self::TYPE_METHOD_ARRAY:
-                $this->class        = !is_string($callable[0]) ? get_class($callable[0]) : $callable[0];
-                $this->reflection   = new \ReflectionMethod($this->class, $callable[1]);
-                
-                if(is_string($callable[0]) && !$this->reflection->isStatic()){
-                    throw new \InvalidArgumentException(
-                        "Deprecated: non-static method called statically"
-                    );
+                try {
+                    $this->reflection = new \ReflectionMethod($callable, "__invoke");
+                } catch (\ReflectionException $e) {
+                    throw new \LogicException($e->getMessage(), $e->getCode(), $e);
                 }
-                
+
+                $this->object = $callable;
+
                 break;
-            
+
+            case self::TYPE_CLOSURE:
+            case self::TYPE_STRING:
+                try {
+                    $this->reflection = new \ReflectionFunction($callable);
+                } catch (\ReflectionException $e) {
+                    throw new \LogicException($e->getMessage(), $e->getCode(), $e);
+                }
+
+                break;
+
+            case self::TYPE_CLASS_METHOD_STRING:
+                [$class, $method] = explode("::", $callable, 2);
+
+                try {
+                    $this->reflection = new \ReflectionMethod($class, $method);
+                } catch (\ReflectionException $e) {
+                    throw new \LogicException($e->getMessage(), $e->getCode(), $e);
+                }
+
+                break;
+
+            case self::TYPE_CLASS_METHOD_ARRAY:
+            case self::TYPE_CLASS_METHOD_ARRAY_STATIC:
+                try {
+                    $this->reflection = new \ReflectionMethod($callable[0], $callable[1]);
+                } catch (\ReflectionException $e) {
+                    throw new \LogicException($e->getMessage(), $e->getCode(), $e);
+                }
+
+                if (self::TYPE_CLASS_METHOD_ARRAY === $this->type) {
+                    $this->object = $callable[0];
+                }
+
+                break;
+
             default:
                 throw new \LogicException;
         }
+
+        if (
+            self::TYPE_CLASS_METHOD_STRING === $this->type
+            || self::TYPE_CLASS_METHOD_ARRAY_STATIC === $this->type
+        ) {
+            $methodString = self::TYPE_CLASS_METHOD_STRING
+                ? $this->value
+                : "[{$this->value[0]}, {$this->value[1]}]"
+            ;
+
+            if (!$this->reflection->isStatic()) {
+                trigger_error(
+                    "Non-static method {$methodString} should not be called statically.",
+                    E_USER_DEPRECATED
+                );
+            }
+        }
     }
-    
+
     /**
-     * To string
-     * 
-     * @return  string
+     * Returns the callable type.
+     *
+     * @return mixed
      */
-    public function __toString(){
-        return (string)$this->reflection;
-    }
-    
-    /**
-     * 
-     * 
-     * @return  int
-     */
-    public function getType(){
+    public function getType()
+    {
         return $this->type;
     }
-    
+
     /**
-     * 
-     * 
-     * @return  string|null
+     * Returns the callable value.
+     *
+     * @return callable
      */
-    public function getClass(){
-        return $this->class;
-    }
-    
-    /**
-     * 
-     * 
-     * @return  callable
-     */
-    public function getValue(){
+    public function getValue(): callable
+    {
         return $this->value;
     }
-    
+
     /**
-     * 
-     * 
-     * @return  \ReflectionFunction|\ReflectionMethod
+     * Returns the callable value reflection instance.
+     *
+     * @return \ReflectionFunction|\ReflectionMethod
      */
-    public function getReflection(){
+    public function getReflection(): \ReflectionFunctionAbstract
+    {
         return $this->reflection;
     }
-    
-    public function invoke($object = null, ...$args){
-        if($this->reflection instanceof \ReflectionMethod){
-            if(!$this->reflection->isStatic()){
-                if(!is_a($object, $this->getClass())){
-                    throw new \LogicException;
-                }
-                
-                return $this->reflection->invokeArgs($object, $args);
-            }
-            
-            return $this->reflection->invokeArgs(null, $args);
-        }
-        
-        return $this->reflection->invokeArgs($args);
-    }
-    
-    public function invokeArgs($object = null, array $args = []){
-        array_unshift($args, $object);
-        return call_user_func_array([$this, "invoke"], $args);
-    }
-    
-    public function invokeMapedArgs($object = null, array $params = [], $default = null){
-        $args   = [];
-        
-        foreach($this->reflection->getParameters() as $param){
-            $value  = $default;
 
-            if(isset($params[$param->getName()])){
-                $value  = $params[$param->getName()];
-            }else if($param->isDefaultValueAvailable()){
-                $value  = $param->getDefaultValue();
-            }
-
-            $args[] = $value;
+    /**
+     * Invoke callable value, and returns callable value response.
+     *
+     * @param mixed ...$args
+     *
+     * @return mixed
+     */
+    public function invoke(...$args)
+    {
+        if (
+            self::TYPE_CLASS_METHOD_STRING === $this->type
+            || self::TYPE_CLASS_METHOD_ARRAY_STATIC === $this->type
+        ) {
+            return $this->reflection->invoke(null, ...$args);
         }
-        
-        return $this->invokeArgs($object, $args);
+
+        if (
+            self::TYPE_INVOKABLE_OBJECT === $this->type
+            || self::TYPE_CLASS_METHOD_ARRAY === $this->type
+        ) {
+            return $this->reflection->invoke($this->object, ...$args);
+        }
+
+        return $this->reflection->invoke(...$args);
     }
 }
